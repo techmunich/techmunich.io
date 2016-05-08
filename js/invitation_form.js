867da617911705431f2493bc6adb1c86faa6658c
stehefan <lier.stefan@gmail.com>
2016-05-08 17:28:43 +0200

    Move form script to vanilla JS

---
+++ js/invitation_form.js
@@ -0,0 +1,47 @@
+'use strict';
+
+(function () {
+	var INVITER_URL = 'https://tech-munich-inviter.herokuapp.com/invitations';
+	var NOTIFICATION_VISIBILITY_CLASS = 'shown';
+	var mailFormElementIdentifier = 'invitation[email]';
+
+	var invitationForm = document.querySelector('#invitationForm');
+	var notificationSuccess = document.querySelector('#invitationFormSuccess');
+	var notificationFail = document.querySelector('#invitationFormFail');
+
+	function restoreViews() {
+		notificationSuccess.classList.remove(NOTIFICATION_VISIBILITY_CLASS);
+		notificationFail.classList.remove(NOTIFICATION_VISIBILITY_CLASS);
+	}
+
+	invitationForm.addEventListener('submit', function (event) {
+		event.preventDefault();
+
+		restoreViews();
+
+		var request = new XMLHttpRequest();
+		var mail = invitationForm.elements[mailFormElementIdentifier].value;
+		var encodedMail = encodeURIComponent(mail);
+		var encodedIdentifier = encodeURIComponent(mailFormElementIdentifier);
+		var encodedParams = encodedIdentifier + "=" + encodedMail;
+
+		request.open('POST', INVITER_URL, true);
+		request.onload = function () {
+			if (request.status >= 200 && request.status < 400) {
+				notificationSuccess.classList.add(NOTIFICATION_VISIBILITY_CLASS);
+			} else {
+				notificationFail.classList.add(NOTIFICATION_VISIBILITY_CLASS);
+			}
+			invitationForm.disabled = false;
+		};
+
+		request.onerror = function () {
+			notificationFail.classList.add(NOTIFICATION_VISIBILITY_CLASS);
+			invitationForm.disabled = false;
+		};
+
+		invitationForm.disabled = true;
+
+		request.send(encodedParams);
+	});
+})();
